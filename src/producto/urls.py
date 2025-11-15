116b44aebef77d328f65179a3b58b0fd4a4a944f
Guillermo Costamaga <costamagaguillermo@gmail.com>
2025-11-15 18:11:01 -0300

    CRUD - FORMULARIOS

--- src/producto/urls.py
+++ src/producto/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('categoria/list/', views.categoria_list, name='categoria_list'),
     path('categoria/create/', views.categoria_create, name='categoria_create'),
+    path('categoria/update/<int:pk>', views.categoria_update, name='categoria_update'),
+    path('categoria/delete/<int:pk>', views.categoria_delete, name='categoria_delete'),
 ]   
--- src/producto/views.py
+++ src/producto/views.py
@@ -1,17 +1,44 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 from . import models, forms 
 
 # Create your views here.
 def index(request):
     return render(request, "producto/index.html")
 
-
+# CATEGORIA - LISTVIEW
 def categoria_list(request):
-    categorias=models.Categoria.objects.all()
-    context={"categorias":categorias}
+    queryset=models.Categoria.objects.all()
+    context={"object_list":queryset}
     return render(request, "producto/categoria_list.html", context)
-    
+
+# CATEGORIA - CREATEVIEW    
 def categoria_create(request):
-    form = forms.CategoriaForm()
+    if request.method == "GET":
+        form = forms.CategoriaForm()
+    if request.method == "POST":
+        form = forms.CategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("producto:categoria_list")
     return render(request, "producto/categoria_form.html", {"form":form}) 
+
+# CATEGORIA - UPDATEVIEW
+def categoria_update (request, pk:int):
+    query = models.Categoria.objects.get(id=pk)
+    if request.method == "GET":
+        form = forms.CategoriaForm(instance=query)
+    if request.method == "POST":
+        form = forms.CategoriaForm(request.POST, instance=query)
+        if form.is_valid():
+            form.save()
+            return redirect('producto:categoria_list')
+    return render(request, "producto/categoria_form.html", {"form":form})
+
+# CATEGORIA - DELETEVIEW
+def categoria_delete (request, pk:int):
+    query = models.Categoria.objects.get(id=pk)
+    if request.method == "POST":
+        query.delete()
+        return redirect('producto:categoria_list')
+    return render(request, "producto/categoria_confirm_delete.html", {"object":query})
